90878c603bb998ddc3bf59cacdc716b581fccaa2
tadeojcocucci <tadeojcocucci@gmail.com>
2023-02-21 12:26:43 -0300

    profile pic placeholder text

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <>
+      <div>
+        <img className="profile"
+          src="../../public/static/profile.jpg"
+          alt="Profile picture" />
+      </div>
+      <div>
+        Hola! Soy Tadeo, me dedico al software. Mi trayectoria empezó como Licenciado en Matemática y luego me doctoré en Ciencias de la Computación. Me interesa sobre todo el desarrollo de videojuegos, el desarrollo web, la ciencia de datos y la inteligencia artificial.
+      </div>
+    </>
   );
 }
