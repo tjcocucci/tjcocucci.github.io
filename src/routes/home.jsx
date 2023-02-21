39926948f8411247c2b47998198f516d9b1e9c0b
tadeojcocucci <tadeojcocucci@gmail.com>
2023-02-21 15:09:45 -0300

    refactor

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -2,11 +2,6 @@ export default function Home() {
   return (
     <>
       <div>
-        <img className="profile"
-          src="../../public/static/profile.jpg"
-          alt="Profile picture" />
-      </div>
-      <div>
         Hola! Soy Tadeo, me dedico al software. Mi trayectoria empezó como Licenciado en Matemática y luego me doctoré en Ciencias de la Computación. Me interesa sobre todo el desarrollo de videojuegos, el desarrollo web, la ciencia de datos y la inteligencia artificial.
       </div>
     </>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,30 +1,14 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Outlet, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavigationBar from "../components/NavigationBar.jsx";
+import LeftColumn from "../components/LeftColumn.jsx";
+import RightColumn from "../components/RightColumn.jsx";
 
 export default function Root() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/" title="Home">Home</Nav.Link>
-              <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
-              <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
-                <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
-                <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
-                <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Outlet />
+      <NavigationBar />
+      <LeftColumn />
+      <RightColumn />
     </div>
   );
 }
