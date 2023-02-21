81da5e1e7fc91ccff11bf53037da8b02efc1f331
tadeojcocucci <tadeojcocucci@gmail.com>
2023-02-21 11:51:49 -0300

    dropdown with placeholder links

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet, Link } from "react-router-dom";
 
@@ -14,6 +15,11 @@ export default function Root() {
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/" title="Home">Home</Nav.Link>
               <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
+              <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
+                <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
+                <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
+                <Nav.Link as={Link} to="/about" title="About">About</Nav.Link>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
