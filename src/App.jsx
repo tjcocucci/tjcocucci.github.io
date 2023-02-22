174c7376713af84b71d268758b3917b3117f507c
tadeojcocucci <tadeojcocucci@gmail.com>
2023-02-22 10:46:18 -0300

    trying again with browser router + 404 redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './components/Router.jsx';
 
 function App() {
   return (
     <div className="App">
-        <HashRouter >
+        <BrowserRouter >
           <Router />
-        </HashRouter>
+        </BrowserRouter>
     </div>
   );
 }
